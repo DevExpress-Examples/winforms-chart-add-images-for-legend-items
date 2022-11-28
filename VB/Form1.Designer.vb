839980f7db3d4f1e2bb6428f610445b06f5ac73b
DevExpressExampleBot <fakeEmail>
2022-11-28 11:06:32 +0400

    Source auto update [skip ci]

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -1,55 +1,55 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
 Namespace ImageLegendItems
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
+
+            MyBase.Dispose(disposing)
+        End Sub
+
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
             Me.components = New System.ComponentModel.Container()
-            Dim XyDiagram1 As DevExpress.XtraCharts.XYDiagram = New DevExpress.XtraCharts.XYDiagram()
-            Dim SideBySideBarSeriesLabel1 As DevExpress.XtraCharts.SideBySideBarSeriesLabel = New DevExpress.XtraCharts.SideBySideBarSeriesLabel()
+            Dim xyDiagram1 As DevExpress.XtraCharts.XYDiagram = New DevExpress.XtraCharts.XYDiagram()
+            Dim sideBySideBarSeriesLabel1 As DevExpress.XtraCharts.SideBySideBarSeriesLabel = New DevExpress.XtraCharts.SideBySideBarSeriesLabel()
             Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
-            Me.NwindDataSet = New nwindDataSet()
-            Me.EmployeesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-            Me.EmployeesTableAdapter = New nwindDataSetTableAdapters.EmployeesTableAdapter()
-            CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(XyDiagram1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(SideBySideBarSeriesLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.NwindDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.EmployeesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.nwindDataSet = New ImageLegendItems.nwindDataSet()
+            Me.employeesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+            Me.employeesTableAdapter = New ImageLegendItems.nwindDataSetTableAdapters.EmployeesTableAdapter()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((xyDiagram1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((sideBySideBarSeriesLabel1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.nwindDataSet), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.employeesBindingSource), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
-            '
-            'chartControl1
-            '
-            Me.chartControl1.DataAdapter = Me.EmployeesTableAdapter
-            Me.chartControl1.DataSource = Me.EmployeesBindingSource
-            XyDiagram1.AxisX.Range.ScrollingRange.SideMarginsEnabled = True
-            XyDiagram1.AxisX.Range.SideMarginsEnabled = True
-            XyDiagram1.AxisX.VisibleInPanesSerializable = "-1"
-            XyDiagram1.AxisY.Range.ScrollingRange.SideMarginsEnabled = True
-            XyDiagram1.AxisY.Range.SideMarginsEnabled = True
-            XyDiagram1.AxisY.VisibleInPanesSerializable = "-1"
-            Me.chartControl1.Diagram = XyDiagram1
+            ' 
+            ' chartControl1
+            ' 
+            Me.chartControl1.DataAdapter = Me.employeesTableAdapter
+            Me.chartControl1.DataSource = Me.employeesBindingSource
+            xyDiagram1.AxisX.Range.ScrollingRange.SideMarginsEnabled = True
+            xyDiagram1.AxisX.Range.SideMarginsEnabled = True
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1"
+            xyDiagram1.AxisY.Range.ScrollingRange.SideMarginsEnabled = True
+            xyDiagram1.AxisY.Range.SideMarginsEnabled = True
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1"
+            Me.chartControl1.Diagram = xyDiagram1
             Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
             Me.chartControl1.Legend.AlignmentHorizontal = DevExpress.XtraCharts.LegendAlignmentHorizontal.Center
             Me.chartControl1.Legend.AlignmentVertical = DevExpress.XtraCharts.LegendAlignmentVertical.TopOutside
@@ -60,49 +60,51 @@ Namespace ImageLegendItems
             Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series(-1) {}
             Me.chartControl1.SeriesTemplate.ArgumentDataMember = "LastName"
             Me.chartControl1.SeriesTemplate.ArgumentScaleType = DevExpress.XtraCharts.ScaleType.Qualitative
-            SideBySideBarSeriesLabel1.LineVisible = True
-            Me.chartControl1.SeriesTemplate.Label = SideBySideBarSeriesLabel1
+            sideBySideBarSeriesLabel1.LineVisible = True
+            Me.chartControl1.SeriesTemplate.Label = sideBySideBarSeriesLabel1
             Me.chartControl1.SeriesTemplate.ValueDataMembersSerializable = "EmployeeID"
-            Me.chartControl1.Size = New System.Drawing.Size(1000, 494)
+            Me.chartControl1.Size = New System.Drawing.Size(946, 474)
             Me.chartControl1.TabIndex = 0
-            '
-            'NwindDataSet
-            '
-            Me.NwindDataSet.DataSetName = "nwindDataSet"
-            Me.NwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-            '
-            'EmployeesBindingSource
-            '
-            Me.EmployeesBindingSource.DataMember = "Employees"
-            Me.EmployeesBindingSource.DataSource = Me.NwindDataSet
-            '
-            'EmployeesTableAdapter
-            '
-            Me.EmployeesTableAdapter.ClearBeforeFill = True
-            '
-            'Form1
-            '
-            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+            AddHandler Me.chartControl1.CustomDrawSeries, New DevExpress.XtraCharts.CustomDrawSeriesEventHandler(AddressOf Me.chartControl1_CustomDrawSeries)
+            ' 
+            ' nwindDataSet
+            ' 
+            Me.nwindDataSet.DataSetName = "nwindDataSet"
+            Me.nwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+            ' 
+            ' employeesBindingSource
+            ' 
+            Me.employeesBindingSource.DataMember = "Employees"
+            Me.employeesBindingSource.DataSource = Me.nwindDataSet
+            ' 
+            ' employeesTableAdapter
+            ' 
+            Me.employeesTableAdapter.ClearBeforeFill = True
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-            Me.ClientSize = New System.Drawing.Size(1000, 494)
+            Me.ClientSize = New System.Drawing.Size(946, 474)
             Me.Controls.Add(Me.chartControl1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            CType(XyDiagram1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(SideBySideBarSeriesLabel1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.NwindDataSet, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.EmployeesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((xyDiagram1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((sideBySideBarSeriesLabel1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.nwindDataSet), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.employeesBindingSource), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
-		#End Region
+#End Region
+        Private chartControl1 As DevExpress.XtraCharts.ChartControl
 
-        Private WithEvents chartControl1 As DevExpress.XtraCharts.ChartControl
-        Friend WithEvents EmployeesTableAdapter As nwindDataSetTableAdapters.EmployeesTableAdapter
-        Friend WithEvents NwindDataSet As nwindDataSet
-        Friend WithEvents EmployeesBindingSource As System.Windows.Forms.BindingSource
+        Private employeesTableAdapter As ImageLegendItems.nwindDataSetTableAdapters.EmployeesTableAdapter
+
+        Private nwindDataSet As ImageLegendItems.nwindDataSet
+
+        Private employeesBindingSource As System.Windows.Forms.BindingSource
     End Class
 End Namespace
-
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+Imports System
 Imports System.Collections.Generic
 Imports System.Data
 Imports System.Drawing
@@ -9,41 +9,40 @@ Imports DevExpress.XtraCharts
 
 Namespace ImageLegendItems
 
-    Partial Public Class Form1
+    Public Partial Class Form1
         Inherits Form
-        Private photos As New Dictionary(Of String, DXImage)()
+
+        Private photos As Dictionary(Of String, DXImage) = New Dictionary(Of String, DXImage)()
 
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-            Me.EmployeesTableAdapter.Fill(Me.NwindDataSet.Employees)
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            employeesTableAdapter.Fill(nwindDataSet.Employees)
             InitializePhotos()
         End Sub
 
         Private Sub InitializePhotos()
-            For Each row As DataRow In NwindDataSet.Employees.Rows
+            For Each row As DataRow In nwindDataSet.Employees.Rows
                 Dim lastName As String = row.ItemArray(1).ToString()
-                If (Not photos.ContainsKey(lastName)) Then
-                    Using stream As New MemoryStream(CType(row.ItemArray(14), Byte()))
+                If Not photos.ContainsKey(lastName) Then
+                    Using stream As MemoryStream = New MemoryStream(CType(row.ItemArray(14), Byte()))
                         Using sourceImage As DXImage = DXImage.FromStream(stream)
-                            Dim image As New DXBitmap(74, 79)
+                            Dim image As DXBitmap = New DXBitmap(74, 79)
                             Using graphics As DXGraphics = DXGraphics.FromImage(image)
-                                graphics.DrawImage(sourceImage,
-                                                   New Rectangle(New Point(5, 5), New Size(75, 75)))
+                                graphics.DrawImage(sourceImage, New Rectangle(New Point(5, 5), New Size(75, 75)))
                             End Using
+
                             photos.Add(lastName, image)
                         End Using
                     End Using
                 End If
-            Next row
+            Next
         End Sub
 
-        Private Sub chartControl1_CustomDrawSeries(ByVal sender As Object, ByVal e As CustomDrawSeriesEventArgs) _
-        Handles chartControl1.CustomDrawSeries
+        Private Sub chartControl1_CustomDrawSeries(ByVal sender As Object, ByVal e As CustomDrawSeriesEventArgs)
             e.DXLegendMarkerImage = photos(e.Series.Name)
         End Sub
     End Class
-
 End Namespace
