cd97d537cc0a9190957d4d4e55e9dc6d9e68aa66
DevExpressExampleBot <fakeEmail>
2022-11-28 11:09:53 +0400

    Source auto update [skip ci]

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+Imports System
 Imports System.Collections.Generic
 Imports System.Data
 Imports System.Drawing
@@ -8,41 +8,40 @@ Imports DevExpress.XtraCharts
 
 Namespace ImageLegendItems
 
-    Partial Public Class Form1
+    Public Partial Class Form1
         Inherits Form
-        Private photos As New Dictionary(Of String, Image)()
+
+        Private photos As Dictionary(Of String, Image) = New Dictionary(Of String, Image)()
 
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-            Me.EmployeesTableAdapter.Fill(Me.NwindDataSet.Employees)
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            employeesTableAdapter.Fill(nwindDataSet.Employees)
             InitializePhotos()
         End Sub
 
         Private Sub InitializePhotos()
-            For Each row As DataRow In NwindDataSet.Employees.Rows
+            For Each row As DataRow In nwindDataSet.Employees.Rows
                 Dim lastName As String = row.ItemArray(1).ToString()
-                If (Not photos.ContainsKey(lastName)) Then
-                    Using stream As New MemoryStream(CType(row.ItemArray(14), Byte()))
-                        Using sourceImage As Image = image.FromStream(stream)
-                            Dim image As New Bitmap(74, 79)
-                            Using graphics As Graphics = graphics.FromImage(image)
-                                graphics.DrawImage(sourceImage, _
-                                                   New Rectangle(New Point(5, 5), New Size(75, 75)))
+                If Not photos.ContainsKey(lastName) Then
+                    Using stream As MemoryStream = New MemoryStream(CType(row.ItemArray(14), Byte()))
+                        Using sourceImage As Image = Image.FromStream(stream)
+                            Dim image As Bitmap = New Bitmap(74, 79)
+                            Using graphics As Graphics = Graphics.FromImage(image)
+                                graphics.DrawImage(sourceImage, New Rectangle(New Point(5, 5), New Size(75, 75)))
                             End Using
+
                             photos.Add(lastName, image)
                         End Using
                     End Using
                 End If
-            Next row
+            Next
         End Sub
 
-        Private Sub chartControl1_CustomDrawSeries(ByVal sender As Object, ByVal e As CustomDrawSeriesEventArgs) _
-        Handles chartControl1.CustomDrawSeries
+        Private Sub chartControl1_CustomDrawSeries(ByVal sender As Object, ByVal e As CustomDrawSeriesEventArgs)
             e.LegendMarkerImage = photos(e.Series.Name)
         End Sub
     End Class
-
 End Namespace
